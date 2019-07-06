ac9aabeb0468965bd0f9f85c51acee4d485f1e90
Agentsealth_Home <thomas1234lint@gmail.com>
2019-07-06 21:38:25 +1000

    Pickup/Drop
    Added Pickup/dropping logic
    Bug
    -When dropping the object losing its scale(??)

--- Team_6_Major_Project/Assets/Scripts/PlayerRayCast.cs
+++ Team_6_Major_Project/Assets/Scripts/PlayerRayCast.cs
@@ -6,7 +6,10 @@ public class PlayerRayCast : MonoBehaviour
 {
     [SerializeField]
     private float distanceToSee = 3.0f;
-
+    [SerializeField]
+    private Transform playerHand;
+    [SerializeField]
+    private int objectInHand;
     RaycastHit hit;
     // Start is called before the first frame update
     void Start()
@@ -18,10 +21,40 @@ public class PlayerRayCast : MonoBehaviour
     void Update()
     {
         Debug.DrawRay(this.transform.position, this.transform.forward * distanceToSee, Color.red);
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastDown();
+        }
+        else if(Input.GetMouseButtonUp(0))
+        {
+            RaycastUp();
+        }
+    }
+
+    private void RaycastDown()
+    {
+        Debug.DrawRay(this.transform.position, this.transform.forward* distanceToSee, Color.red);
         if(Physics.Raycast(this.transform.position, this.transform.forward, out hit, distanceToSee))
         {
             Debug.Log("Touched " + hit.collider.gameObject.name);
-            Destroy(hit.collider.gameObject);
+            if (hit.collider.gameObject.tag == "Iron Ore")
+            {
+                hit.collider.gameObject.GetComponent<Rigidbody>().useGravity = false;
+                hit.collider.gameObject.transform.position = playerHand.position;
+                hit.collider.gameObject.transform.parent = playerHand.transform;
+                objectInHand++;
+            }
+        }
+    }
+
+    private void RaycastUp()
+    {
+       if(objectInHand >0)
+        {
+            hit.collider.gameObject.GetComponent<Rigidbody>().useGravity = true;
+            playerHand.GetChild(0).gameObject.transform.localScale = new Vector3(1,1,1);
+            playerHand.GetChild(0).gameObject.transform.parent = null;
+            objectInHand--;
         }
     }
 }
