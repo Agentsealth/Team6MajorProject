6ca2eb5dc107f0152d4180be20402a4db619aa1c
Agentsealth <thomas1234lint@gmail.com>
2019-07-25 15:14:33 +1000

    Shop
    -Added Scripts
    Player Stats
    Ore
    Shop
    -Added Materials
    Test_Steel
    Test_Bronze

---
+++ Team_6_Major_Project/Assets/Scripts/Shop.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Shop : MonoBehaviour
+{
+    public int ironCost;
+    public int bronzeCost;
+    public int steelCost;
+
+    public Text ironCostText;
+    public Text bronzeCostText;
+    public Text steelCostText;
+
+    public PlayerStats playerStats;
+
+    public GameObject ore;
+
+    public Transform drop;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerStats = FindObjectOfType<PlayerStats>();
+        ironCostText.text = "Iron Ore (" + ironCost + ")";
+        bronzeCostText.text = "Bronze Ore (" + bronzeCost + ")";
+        steelCostText.text = "Steel Ore (" + steelCost + ")";
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void BuyIron()
+    {
+        if(playerStats.gold >= ironCost)
+        {
+            playerStats.gold -= ironCost;
+            ore.GetComponent<Ore>().material = Ore.OreMaterial.iron;
+            Instantiate(ore, drop.position, Quaternion.identity);
+        }
+    }
+
+    public void BuyBronze()
+    {
+        if (playerStats.gold >= bronzeCost)
+        {
+            playerStats.gold -= bronzeCost;
+            ore.GetComponent<Ore>().material = Ore.OreMaterial.bronze;
+            Instantiate(ore, drop.position, Quaternion.identity);
+
+        }
+    }
+
+    public void BuySteel()
+    {
+        if (playerStats.gold >= steelCost)
+        {
+            playerStats.gold -= steelCost;
+            ore.GetComponent<Ore>().material = Ore.OreMaterial.steel;
+            Instantiate(ore, drop.position, Quaternion.identity);
+
+        }
+    }
+}
