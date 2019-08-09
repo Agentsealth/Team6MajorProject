058a12568c46f4250335a6ecc92cd3dc13a41357
CampbellCKJ <43637015+CampbellCKJ@users.noreply.github.com>
2019-08-09 14:13:45 +1000

    Added Main Menu with Loading Screen Stuff
    I think.
    Loading is too fast/staggering to note whether it fully works.

---
+++ Team_6_Major_Project/Assets/LoadingScreenControl.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class LoadingScreenControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject loadingScreenObj;
+    public GameObject loadingMan;
+    public Slider slider;
+
+    AsyncOperation async;
+    public void LoadScreenExample()
+    {
+        StartCoroutine(LoadingScreen());
+    }
+
+    IEnumerator LoadingScreen()
+    {
+        loadingScreenObj.SetActive(true);
+        async = SceneManager.LoadSceneAsync("ssScene");
+        async.allowSceneActivation = false;
+
+        while (async.isDone == false)
+        {
+            slider.value = async.progress;
+            if (async.progress == 0.9f)
+            {
+                slider.value = 1f;
+                async.allowSceneActivation = true;
+            }
+            yield return null;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
