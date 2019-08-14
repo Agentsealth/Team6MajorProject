13508c4789a4ffac5f51f32a2571d0aeddad3f1a
Agentsealth <thomas1234lint@gmail.com>
2019-08-14 13:33:35 +1000

    Texture change

--- Team_6_Major_Project/Assets/Scripts/Sword.cs
+++ Team_6_Major_Project/Assets/Scripts/Sword.cs
@@ -14,16 +14,68 @@ public class Sword : MonoBehaviour
     public MaterialHandle materialHandle;
     public MaterialGuard materialGuard;
 
+    public Material[] textures;
+
     public int quality;
     // Start is called before the first frame update
     void Start()
     {
-        
+        TextureChangeBlade();
+        TextureChangeGuard();
+        TextureChangeHandle();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    void TextureChangeBlade()
+    {
+        if (materialBlade == MaterialBlade.iron)
+        {
+            gameObject.transform.GetChild(2).GetComponent<MeshRenderer>().material = textures[0];
+        }
+        else if (materialBlade == MaterialBlade.steel)
+        {
+            gameObject.transform.GetChild(2).GetComponent<MeshRenderer>().material = textures[1];
+        }
+        else if (materialBlade == MaterialBlade.bronze)
+        {
+            gameObject.transform.GetChild(2).GetComponent<MeshRenderer>().material = textures[2];
+        }
+    }
+
+    void TextureChangeGuard()
+    {
+        if (materialGuard == MaterialGuard.iron)
+        {
+            gameObject.transform.GetChild(1).GetComponent<MeshRenderer>().material = textures[0];
+        }
+        else if (materialGuard == MaterialGuard.steel)
+        {
+            gameObject.transform.GetChild(1).GetComponent<MeshRenderer>().material = textures[1];
+        }
+        else if (materialGuard == MaterialGuard.bronze)
+        {
+            gameObject.transform.GetChild(1).GetComponent<MeshRenderer>().material = textures[2];
+        }
+    }
+
+    void TextureChangeHandle()
+    {
+        if (materialHandle == MaterialHandle.iron)
+        {
+            gameObject.transform.GetChild(0).GetComponent<MeshRenderer>().material = textures[0];
+        }
+        else if (materialHandle == MaterialHandle.steel)
+        {
+            gameObject.transform.GetChild(0).GetComponent<MeshRenderer>().material = textures[1];
+        }
+        else if (materialHandle == MaterialHandle.bronze)
+        {
+            gameObject.transform.GetChild(0).GetComponent<MeshRenderer>().material = textures[2];
+        }
     }
 }
