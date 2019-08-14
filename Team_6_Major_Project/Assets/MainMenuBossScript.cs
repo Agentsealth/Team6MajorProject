971822cd67ddd722e013aa4fe110a0790895fbd3
CampbellCKJ <43637015+CampbellCKJ@users.noreply.github.com>
2019-08-14 11:39:18 +1000

    Menu Stuff & New Sword Prefabs

--- Team_6_Major_Project/Assets/MainMenuBossScript.cs
+++ Team_6_Major_Project/Assets/MainMenuBossScript.cs
@@ -4,11 +4,24 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class MainMenuBossScript : MonoBehaviour
 {
+    public GameObject MainMenu;
+    public GameObject Options;
     // Start is called before the first frame update
     public void PlayGame()
     {
         SceneManager.LoadScene("ssScene");
     }
 
+    public void ShowOptions()
+    {
+        MainMenu.SetActive(false);
+        Options.SetActive(true);
+    }
+
+    public void ShowMenu()
+    {
+        MainMenu.SetActive(true);
+        Options.SetActive(false);
+    }
 
 }
---
+++ Team_6_Major_Project/Assets/OptionsMaster.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class OptionsMaster : MonoBehaviour
+{
+
+    public Slider MasterVolumeSlider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MasterVolumeSlider.value = PlayerPrefs.GetFloat("MasterVolume");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AudioListener.volume = MasterVolumeSlider.value;
+    }
+
+
+    public void ApplyChanges()
+    {
+        PlayerPrefs.SetFloat("MasterVolume", MasterVolumeSlider.value);
+        PlayerPrefs.Save();
+    }
+
+    
+}
