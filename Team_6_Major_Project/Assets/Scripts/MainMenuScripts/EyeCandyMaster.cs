e9d204110da7026703ef25703b2c17b3ae353688
CampbellCKJ <43637015+CampbellCKJ@users.noreply.github.com>
2019-08-15 11:43:53 +1000

    Main Menu changes & eye candy.
    + Moved scripts to proper folder.

---
+++ Team_6_Major_Project/Assets/Scripts/MainMenuScripts/EyeCandyMaster.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+public class EyeCandyMaster : MonoBehaviour
+{
+
+    public GameObject HLSparks;
+
+    public GameObject Button1;
+    public GameObject Button2;
+    public GameObject Button3;
+    public GameObject Button4;
+    public GameObject Button5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    
+    }
+
+    public void PlayGameCandy()
+    {
+        HLSparks.SetActive(true);
+        HLSparks.transform.localPosition = new Vector3(0f, 0.295f, -0.57f);
+    }
+
+    public void OptionsCandy()
+    {
+        HLSparks.SetActive(true);
+        HLSparks.transform.localPosition = new Vector3(0f, -0.099f, -0.57f);
+    }
+
+    public void ExitCandy()
+    {
+        HLSparks.SetActive(true);
+        HLSparks.transform.localPosition = new Vector3(0f, -0.482f, -0.57f);
+    }
+
+    public void KillCandy()
+    {
+        HLSparks.SetActive(false);
+    }
+
+}
---
+++ Team_6_Major_Project/Assets/Scripts/MainMenuScripts/PlayGameHighlighted.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+public class PlayGameHighlighted : MonoBehaviour
+{
+    public GameObject HLSparks;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        HLSparks.SetActive(true);
+        HLSparks.transform.position = new Vector3(0f, 0.295f, -0.57f);
+    }
+
+    private void OnMouseExit()
+    {
+        HLSparks.SetActive(false);
+    }
+}
