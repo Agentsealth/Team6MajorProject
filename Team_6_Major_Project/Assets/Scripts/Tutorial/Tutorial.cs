af1fe836a5af575c5d9d7f0a7730575e216f4ad9
glittika-gerdmanee <43663001+glittika-gerdmanee@users.noreply.github.com>
2019-09-18 23:22:50 +1000

    Setting up Tutorial Script

---
+++ Team_6_Major_Project/Assets/Scripts/Tutorial/Tutorial.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Tutorial : MonoBehaviour
+{
+    public GameObject tutPanel01, tutPanel02, tutPanel03; //make array later or something idk
+    //public GameObject[] tutPanels = new GameObject[1];
+    [TextArea(3, 5)]
+    public string[] texts = new string[1]; //array of strings for Tutorial panel
+    public int textPos = 0; //current position in string array
+    public int panelPos = 0;
+    public Text currentText = null; //String currently displayed in Text
+    public Button nextButton = null; //button to move onto next step
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void NextButton()
+    {
+        /*if (panelPos + 1 < tutPanels.Length && textPos + 1 < texts.Length)
+        {
+            ++textPos;
+        }*/
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentText.text = texts[textPos];
+
+    }
+}
