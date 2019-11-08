16ea0ed743afaa37dcb6333da0af7271bf4f08b2
Thomas Nguyen Holo <43657899+ThomasXuanNguyen@users.noreply.github.com>
2019-11-08 11:34:55 +1100

    Commented/Refactored TestDialogueTrigger

--- Team_6_Major_Project/Assets/Scripts/CustomerScript/TestDialogueTrigger.cs
+++ Team_6_Major_Project/Assets/Scripts/CustomerScript/TestDialogueTrigger.cs
@@ -246,67 +246,91 @@ public class TestDialogueTrigger : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Gets the distance between the gameObject and the player
         dist = Vector3.Distance(gameObject.transform.position, playerStats.gameObject.transform.position);
+        //Checks if the distance is less than or equal to 3.2
         if(dist <= 3.2)
         {
+            //Sets the bool inRange to true
             inRange = true;
         }
         else
         {
+            //Sets the bool inRange to false
             inRange = false;
         }
-
+        //Checks if the bool delayCustomer is true
         if(delayCustomer == true)
         {
+            //Decreases delayInteraction by one each frame
             delayInteraction--;
         }
-
+        //Checks if the bool delayAi is true
         if(delayAi == true)
         {
+            //Decreases delayAiInteraction by one each frame
             delayAiInteraction--;
         }
-
+        //Checks if the bool inRange is true
         if(inRange == true)
         {
+            //Checks if the dialogueDoneforDay is false
             if (dialogueDoneforDay == false)
             {
+                //Checks if space is pressed down and dialogueStart is false
                 if (Input.GetKeyDown(KeyCode.Space) && dialogueStart == false)
                 {
-                    TriggerDialogue();                   
+                    //Runs the function trigger Dialogue
+                    TriggerDialogue();
+                    //Sets the customer Number based on the playerStats customerOrderNumber
                     CustomerNumber = playerStats.CustomerOrderNumber;
+                    //Increases the playerStats customerOrderNumber by one
                     playerStats.CustomerOrderNumber++;
+                    //Sets the bool inDialogue based on the dialogueManager inchat bool
                     inDialogue = dialogueManager.inChat;
+                    //Sets the bool dialogueStart to true
                     dialogueStart = true;
                 }
+                //Checks if the space is pressed down
                 else if (Input.GetKeyDown(KeyCode.Space))
                 {
+                    //Runs the DisplayNextSentence function
                     dialogueManager.DisplayNextSentence();
+                    //Sets the bool inDialogue based on the dialogueManager inchat bool
                     inDialogue = dialogueManager.inChat;
+                    //Checks if the bool inDialogue is false
                     if (inDialogue == false)
                     {
+                        //Checks if the text index in the tutorial script is equal to 1
                         if (tut.textPos == 1)
                         {
+                            //Runs the function Progress tutorial in the tutorial script to index 1
                             tut.ProgressTutorial(1);
-
                         }
-
+                        //Runs the WaypointUpdate function
                         WaypointUpdate();
+                        //Runs the setSlotWayPoint function
                         customerAI.setSlotWayPoint();
+                        //Checks if the waypoint1Slot is equal to 1
                         if(customerAI.waypoint1Slot == 1)
                         {
+                            //Sets the first waypoint array at index 0 to empty
                             customerAI.waypointManager.waypoint1_1 = "Empty";
                         }
+                        //Runs the waypoint1Empty function
                         customerAI.waypointManager.Waypoint1Empty();
-                        //Waypoint slot is empty
+                        //Sets the bool dialogueDoneforDay to true
                         dialogueDoneforDay = true;
+                        //Sets the bool dialogueStart to false
                         dialogueStart = false;
+                        //Runs the SetItemSlot function
                         SetItemSlot();
                         return;
                     }
                 }
                 else
                 {
-                    //Waypoint slot is full
+                    //return;
                 }
             }
             else
@@ -314,9 +338,10 @@ public class TestDialogueTrigger : MonoBehaviour
                 return;
             }
         }
-
+        //Checks if the waypointIndex is equal to 4
         if (customerAI.waypointIndex == 4)
         {
+            //Runs the EndDialogue function
             dialogueManager.EndDialogue();
         }
 
@@ -325,13 +350,19 @@ public class TestDialogueTrigger : MonoBehaviour
 
     public void SetItemSlot()
     {
+        //A for loop which uses i based on the slot's length
         for (int i = 0; i < slots.Length; i++)
         {
+            //Checks the customerNumber for each of the slot's slotNumber
             if (CustomerNumber == slots[i].slotNumber)
             {
+                //Sets the slotNumber as the slots
                 SlotNumber = slots[i];
+                //Sets the docketText to be the blade
                 SlotNumber.docketBlade.text = dialogue.bladeType.ToString() + " " + dialogue.bladeMaterial.ToString() + " blade.";
+                //Sets the docketText to be the guard
                 SlotNumber.docketGuard.text = dialogue.guardMaterial.ToString() + " guard ";
+                //Sets the docketText to be the handle
                 SlotNumber.docketHandle.text = dialogue.handleMaterial.ToString() + " handle";
             }
         }
@@ -339,10 +370,13 @@ public class TestDialogueTrigger : MonoBehaviour
 
     public void CheckItemSlot()
     {
+        //A for loop which uses i based on the slot's length
         for (int i = 0; i < slots.Length; i++)
         {
+            //Checks the customerNumber for each of the slot's slotNumber
             if (CustomerNumber == slots[i].slotNumber)
             {
+                //Runs the function CheckItem
                 CheckItem();
             }
         }
@@ -350,111 +384,123 @@ public class TestDialogueTrigger : MonoBehaviour
 
     public void CheckItem()
     {
-        if(SlotNumber.bladeType == dialogue.bladeType)
+        //Checks if the slot's bladeType is the same as the dialogue's bladetype and
+        // slot's bladeMaterial is the same as the dialogue's bladeMaterial and
+        // slot's guardMaterial is the same as the dialogue's guardMaterial and
+        // slot's handleMaterial is the same as the dialogue's handleMaterial
+        if (SlotNumber.bladeType == dialogue.bladeType && SlotNumber.bladeMaterial == dialogue.bladeMaterial &&
+            SlotNumber.guardMaterial == dialogue.guardMaterial && SlotNumber.handleMaterial == dialogue.handleMaterial)
         {
-            if(SlotNumber.bladeMaterial == dialogue.bladeMaterial)
+            //Checks if the bool delayCustomer is equal to false
+            if (delayCustomer == false)
             {
-                if(SlotNumber.guardMaterial == dialogue.guardMaterial)
-                {
-                    if(SlotNumber.handleMaterial == dialogue.handleMaterial)
-                    {                     
-                                              
-                            if(delayCustomer == false)
-                            {
-                                delayCustomer = true;
-                            }
-
-                            if (delayAi == false && delayInteraction <= 0)
-                            {
-                                delayAi = true;
-                            }
-
-                            if (delayInteraction > 0)
-                            {
-
-                            }
-                            else
-                            {                               
-                                if (delayAiInteraction > 0)
-                                {
-
-                                }
-                                else
-                                {
-                                    customerAI.waypointIndex++;
-                                }
-
-                                delayCustomer = false;
-
-                                if (delayCustomer == false)
-                                {
-                                    if(image > 0)
-                                    {
-                                        return;
-                                    }
-                                    else
-                                    {
-                                        image = 1;
-                                        quality = SlotNumber.quality;
-                                        Tip();
-                                        playerStats.gold += cost;
-                                        coinGain.Play();
-                                        ShowFloatingText();
-                                        Banter();                                       
-                                        Destroy(SlotNumber.sword);
-                                        SlotNumber.docketBlade.text = "";
-                                        SlotNumber.docketGuard.text = "";
-                                        SlotNumber.docketHandle.text = "";
-
-                                    }
-                                        
-                                }
-                                
-                            }                     
-                    }
-                    else
-                    {
-                        SlotNumber.sword.transform.position = SlotNumber.badlocation.position;
-                    }
-                }
-                else
-                {
-                    SlotNumber.sword.transform.position = SlotNumber.badlocation.position;
-                }
+                //Sets the delayCustomer to true
+                delayCustomer = true;
+            }
+            //Checks if the bool delayAi is equal to false and delayInteraction is less than or equal to 0
+            if (delayAi == false && delayInteraction <= 0)
+            {
+                //Sets the delayAi to true
+                delayAi = true;
+            }
+            //Checks if the delayInteraction is greater than 0
+            if (delayInteraction > 0)
+            {
+                return;
             }
             else
             {
-                SlotNumber.sword.transform.position = SlotNumber.badlocation.position;
+                //Checks if the delayAiInteraction is greater than 0
+                if (delayAiInteraction > 0)
+                {
+                    return;
+                }
+                else
+                {
+                    //Increases the waypointIndex by one
+                    customerAI.waypointIndex++;
+                }
+                //Sets bool delayCustomer to false
+                delayCustomer = false;
+                //CHecks if the bool delayCustomer is equal to false
+                if (delayCustomer == false)
+                {
+                    //Checks if image is greater than 0
+                    if (image > 0)
+                    {
+                        return;
+                    }
+                    else
+                    {
+                        //Sets image to 1
+                        image = 1;
+                        //Sets the quality to be the slotnumber's quality
+                        quality = SlotNumber.quality;
+                        //Runs the tip functions
+                        Tip();
+                        //Adds the cost of the sword to the player's gold
+                        playerStats.gold += cost;
+                        //Plays audio source
+                        coinGain.Play();
+                        //Runs the ShowFloatingText function
+                        ShowFloatingText();
+                        //Runs the Banter function
+                        Banter();
+                        //Destory the slot's sword gameObjecy
+                        Destroy(SlotNumber.sword);
+                        //Sets the text to blank
+                        SlotNumber.docketBlade.text = "";
+                        //Sets the text to blank
+                        SlotNumber.docketGuard.text = "";
+                        //Sets the text to blank
+                        SlotNumber.docketHandle.text = "";
+
+                    }
+
+                }
+
             }
         }
         else
         {
+            //Sets the sword position to a badLocation's position
             SlotNumber.sword.transform.position = SlotNumber.badlocation.position;
         }
     }
 
     public void ShowFloatingText()
     {
+        //Creates an Image GameObject 
         GameObject var = Instantiate(floatingImage, imagePosition.position, Quaternion.identity, transform);
+        //Checks if the quality is greater than 0 and less than 25
         if (quality >= 0 && quality < 25)
         {
+            //Sets the sprite on the spriteReneder to a badEmo sprite
             var.GetComponent<SpriteRenderer>().sprite = badEmo;
+            //Sets the randomBadText int based on a range between 1 and the badQualitySentences's length
             randombadtext = Random.Range(1, dialogueManager.badQualitySentences.Length);
+            //Sets the sentence in the 4 index to the badQualitySentences from randomBadText int
             dialogue.sentences[4] = dialogueManager.badQualitySentences[randombadtext];
 
         }
         else if (quality >= 25 && quality < 75)
         {
+            //Sets the sprite on the spriteReneder to a neutralEmo sprite
             var.GetComponent<SpriteRenderer>().sprite = neutralEmo;
+            //Sets the randomneturaltext int based on a range between 1 and the neturalQualitySentences's length
             randomneturaltext = Random.Range(1, dialogueManager.neturalQualitySentences.Length);
+            //Sets the sentence in the 4 index to the neturalQualitySentences from randomneturaltext int
             dialogue.sentences[4] = dialogueManager.neturalQualitySentences[randomneturaltext];
 
 
         }
         else if (quality >= 75 && quality <= 100)
         {
+            //Sets the sprite on the spriteReneder to a goodEmo sprite
             var.GetComponent<SpriteRenderer>().sprite = goodEmo;
+            //Sets the randomgoodtext int based on a range between 1 and the bestQualitySentences's length
             randomgoodtext = Random.Range(1, dialogueManager.bestQualitySentences.Length);
+            //Sets the sentence in the 4 index to the bestQualitySentences from randomgoodtext int
             dialogue.sentences[4] = dialogueManager.bestQualitySentences[randomgoodtext];
 
 
@@ -463,47 +509,30 @@ public class TestDialogueTrigger : MonoBehaviour
 
     public void Banter()
     {
+        //Runs the function banterDialogue with the variable dialogue and 4
         dialogueManager.BanterDialogue(dialogue, 4);
     }
 
     public void TriggerDialogue()
     {
+        //Runs the function startDialogue with the variable dialogue
          dialogueManager.StartDialogue(dialogue);
     }
 
     public void WaypointUpdate()
     {
+        //Checks if the waypoint index is equal to 3
         if (customerAI.waypointIndex == 3)
         {
+            //Runs the CheckItem function
             CheckItem();
         }             
         else
         {
+            //Increases waypointIndex by 1
             customerAI.waypointIndex++;
         }
     }
 
-    //private void OnTriggerEnter(Collider other)
-    //{
-    //    if(other.gameObject.tag == "Iron Sword")
-    //    {
-    //        Debug.Log(other.ToString());
-    //        if(other.gameObject.GetComponent<Sword>().swordType == dialogue.bladeType)
-    //        {
-    //            if(other.gameObject.GetComponent<Sword>().materialBlade == dialogue.bladeMaterial)
-    //            {
-    //                if (other.gameObject.GetComponent<Sword>().materialGuard == dialogue.guardMaterial)
-    //                {
-    //                    if (other.gameObject.GetComponent<Sword>().materialHandle == dialogue.handleMaterial)
-    //                    {
-    //                        playerStats.gold += gold;
-    //                        Destroy(other.gameObject);
-    //                        WaypointUpdate();
-    //                    }
-    //                }
-    //            }
-    //        }
-    //    }
-    //}
 }
 
